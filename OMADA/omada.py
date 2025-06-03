41e4c2912bf17233c202ff1edede5d790ab8afbb
TI - DANIEL PEREIRA <159944510+danielspereiraunifique@users.noreply.github.com>
2025-06-03 18:44:23 -0300

    Create omada.py

---
+++ OMADA/omada.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+
+import requests
+import json
+import urllib3
+
+# Desativar avisos de SSL
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Definir variáveis
+OMADA_URL = "https://wifibusiness.unifique.com.br"
+USERNAME = "admin"
+PASSWORD = "Bt92rVzZhczSYZ%v"
+
+# Função para obter o controller ID
+def get_controller_id():
+    response = requests.get(f"{OMADA_URL}/api/info", verify=False)
+    return response.json()['result']['omadacId']
+
+# Função para login e obter o token
+def login(controller_id):
+    login_url = f"{OMADA_URL}/{controller_id}/api/v2/login"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "username": USERNAME,
+        "password": PASSWORD
+    }
+    session = requests.Session()
+    response = session.post(login_url, headers=headers, data=json.dumps(data), verify=False)
+    token = response.json()['result']['token']
+    return session, token
+
+# Função para obter a lista de sites
+def get_sites(controller_id, session, token):
+    sites_url = f"{OMADA_URL}/{controller_id}/api/v2/sites?token={token}&currentPage=1&currentPageSize=1000"
+    headers = {
+        "Content-Type": "application/json",
+        "Csrf-Token": token
+    }
+    response = session.get(sites_url, headers=headers, verify=False)
+    return response.json()
+
+# Função para obter a lista de devices de um site
+def get_devices(controller_id, session, token, site_id):
+    devices_url = f"{OMADA_URL}/{controller_id}/api/v2/sites/{site_id}/devices?token={token}"
+    headers = {
+        "Content-Type": "application/json",
+        "Csrf-Token": token
+    }
+    response = session.get(devices_url, headers=headers, verify=False)
+    return response.json()
+
+# Fluxo principal
+controller_id = get_controller_id()
+session, token = login(controller_id)
+sites_info = get_sites(controller_id, session, token)
+
+# Lista para armazenar o resultado final
+result = []
+
+# Verificação da estrutura de sites_info
+if 'result' in sites_info and 'data' in sites_info['result']:
+    # Filtrar apenas os campos 'id' e 'name' dos sites
+    filtered_sites = [{"id": site["id"], "name": site["name"]} for site in sites_info['result']['data']]
+
+    # Para cada site, obter os dispositivos
+    for site in filtered_sites:
+        site_id = site['id']
+        site_name = site['name']
+
+        # Obter os dispositivos do site
+        devices_info = get_devices(controller_id, session, token, site_id)
+        #print(json.dumps(devices_info, indent=4))
+        # Verificar se a resposta contém os dispositivos corretamente
+        if 'result' in devices_info:
+            # Acessar a lista de dispositivos diretamente no campo 'result'
+            for device in devices_info['result']:
+                device_type = device.get('type')
+                device_nome = device.get('name')
+                device_mac = device.get('mac')
+                device_ip = device.get('ip')
+                device_publicip = device.get('publicIp')
+                device_uptime = device.get('uptime')
+                device_status = device.get('statusCategory')
+                device_cpuutil = device.get('cpuUtil')
+                device_memutil =  device.get('memUtil')
+                device_download = device.get('download')
+                device_upload = device.get('upload')
+                device_clientnum = device.get('clientNum')
+
+                # Adicionar o resultado à lista
+                result.append({
+                    "site_nome": site_name,
+                    "site_id": site_id,
+                    "device_type": device_type,
+                    "device_nome": device_nome,
+                    "device_mac": device_mac,
+                    "device_ip": device_ip,
+                    "device_publicip": device_publicip,
+                    "device_uptime": device_uptime,
+                    "device_status": device_status,
+                    "device_cpuutil": device_cpuutil,
+                    "device_memutil": device_memutil,
+                    "device_download": device_download,
+                    "device_upload": device_upload,
+                    "device_clientnum": device_clientnum
+                })
+
+# Imprimir o JSON final formatado com os campos desejados
+print(json.dumps(result, indent=4))
